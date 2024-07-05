bedf7c488c8b0b12e0963f7ce2318506250a3600
jinheelee04 <59869557+jinheelee04@users.noreply.github.com>
2024-07-05 13:51:18 +0900

    express.static()
    Express의 내장 미들웨어 기능
    이미지, CSS파일 및 JavaScript 파일과 같은 정적 파일 제공

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 const port = 4000;
 app.get('/',(req, res)=>{
     res.send('안녕하세요.1111');
 })
+
+// app.use('/haha', express.static('uploads')); // 정적인 파일 제공, '/haha' 가상경로 지정, 상대경로
+// console.log(path.join(__dirname,'../uploads')); // online-store-app\backend\uploads
+// console.log(path.join(__dirname,'/uploads')); // online-store-app\backend\src\uploads
+app.use(express.static(path.join(__dirname,'../uploads'))); // path 절대경로 사용
 
 app.listen(port, ()=>{
     console.log(`${port}번에서 실행이 되었습니다.`);
