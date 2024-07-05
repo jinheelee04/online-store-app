a6eac7eeb91026b07337cfe8751a41acc403683d
jinheelee04 <59869557+jinheelee04@users.noreply.github.com>
2024-07-05 15:35:07 +0900

    Express.json() 미들웨어 추가

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,8 +5,15 @@ const port = 4000;
 const cors = require('cors')
 
 app.use(cors());
+app.use(express.json()); // Express 애플리케이션에서 JSON 형태의 요청(request) body를 파싱(parse)하기 위해 사용되는 미들웨어(middleware)
+
 app.get('/',(req, res)=>{
     res.send('안녕하세요.1111');
+})
+
+app.post('/', (req, res) =>{
+    console.log(req.body);
+    res.json(req.body);
 })
 
 // app.use('/haha', express.static('uploads')); // 정적인 파일 제공, '/haha' 가상경로 지정, 상대경로
