1f3b1203232d3b5bdf81d9a6e18050226df3d1a4
jinheelee04 <59869557+jinheelee04@users.noreply.github.com>
2024-07-05 10:49:07 +0900

    React Router Dom 적용하기

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,31 @@
+import {Outlet, Route, Routes} from "react-router-dom";
+import LandingPage from "./pages/LandingPage/index.jsx";
+import LoginPage from "./pages/LoginPage/index.jsx";
+import RegisterPage from "./pages/RegisterPage/index.jsx";
+import Navbar from "./layout/NavBar/index.jsx";
+import Footer from "./layout/Footer/index.jsx";
+import './App.css';
+
+function Layout(){
+    return (
+        <div>
+            <Navbar />
+            <main>
+                <Outlet />
+            </main>
+            <Footer />
+        </div>
+    )
+}
 function App() {
   return (
-    <div className="App">
-        <h1 className='text-3xl font-bold underline'>
-            Hello World!
-        </h1>
-    </div>
+    <Routes>
+        <Route path='/' element={<Layout/>}>
+            <Route index element={<LandingPage/>} />
+            <Route path='/login' element={<LoginPage/>} />
+            <Route path='/register' element={<RegisterPage/>} />
+        </Route>
+    </Routes>
   )
 }
 
