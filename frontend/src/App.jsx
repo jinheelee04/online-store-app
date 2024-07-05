e0f59d1a08c88f77170a1dbde7207c4b8636ef10
jinheelee04 <59869557+jinheelee04@users.noreply.github.com>
2024-07-05 11:09:21 +0900

    CSS 전체 구조 및 React Icons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,9 +8,9 @@ import './App.css';
 
 function Layout(){
     return (
-        <div>
+        <div className='flex flex-col h-screen justify-between'>
             <Navbar />
-            <main>
+            <main className='mb-auto w-10/12 max-w-4xl mx-auto'>
                 <Outlet />
             </main>
             <Footer />
