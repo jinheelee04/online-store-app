798f2bd25f3902b6cdcdeac648bbb0ca96e50182
jinheelee04 <59869557+jinheelee04@users.noreply.github.com>
2024-07-05 11:21:03 +0900

    리덕스 사용 준비

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {BrowserRouter} from "react-router-dom";
+import React from "react";
+import {Provider} from "react-redux";
+import {store} from "./store";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </BrowserRouter>
 
 )
