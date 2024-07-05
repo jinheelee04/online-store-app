4e8fed12ba71b380e62dab62c696f1acad0d922d
jinheelee04 <59869557+jinheelee04@users.noreply.github.com>
2024-07-05 15:55:13 +0900

    MonggoDB 사용하기

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,10 +2,22 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 4000;
-const cors = require('cors')
+const cors = require('cors');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
 app.use(cors());
 app.use(express.json()); // Express 애플리케이션에서 JSON 형태의 요청(request) body를 파싱(parse)하기 위해 사용되는 미들웨어(middleware)
+
+mongoose.connect(process.env.MONGGO_URI)
+    .then(()=> {
+        console.log('연결 완료');
+    })
+    .catch(err => {
+        console.err(err);
+    })
+;
 
 app.get('/',(req, res)=>{
     res.send('안녕하세요.1111');
