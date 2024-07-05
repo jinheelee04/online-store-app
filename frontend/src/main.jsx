85db124c3490fb5aaea2a637b0310656bd434810
jinheelee04 <59869557+jinheelee04@users.noreply.github.com>
2024-07-05 13:14:58 +0900

    Redux-Persist

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,15 @@ import './index.css'
 import {BrowserRouter} from "react-router-dom";
 import React from "react";
 import {Provider} from "react-redux";
-import {store} from "./store";
+import {persistor, store} from "./store";
+import {PersistGate} from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
         <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+
+            </PersistGate>
             <App />
         </Provider>
     </BrowserRouter>
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,9 +1,28 @@
-import {configureStore} from "@reduxjs/toolkit";
+import {combineReducers, configureStore} from "@reduxjs/toolkit";
 import userReducer from './userSlice';
+import storage from 'redux-persist/lib/storage';
+import {persistReducer, persistStore} from "redux-persist";
+import {FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE} from "redux-persist/es/constants";
+
+const rootReducer = combineReducers({
+    user: userReducer
+})
+
+const persistConfig = {
+    key: 'root',
+    storage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-    reducer:{
-        user: userReducer
-    }
+    reducer: persistedReducer,
+    middleware : getDefaultMiddleware => getDefaultMiddleware({
+        serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+        }
+    })
 })
+
+export const persistor = persistStore(store);
 
