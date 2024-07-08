1333350f6a14e587d9d4b57abc607d41923f3f61
jinheelee04 <59869557+jinheelee04@users.noreply.github.com>
2024-07-08 12:57:23 +0900

    react toast 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,10 +5,18 @@ import RegisterPage from "./pages/RegisterPage/index.jsx";
 import Navbar from "./layout/NavBar/index.jsx";
 import Footer from "./layout/Footer/index.jsx";
 import './App.css';
+import {ToastContainer} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 function Layout(){
     return (
         <div className='flex flex-col h-screen justify-between'>
+            <ToastContainer
+                position='bottom-right'
+                theme='light'
+                pauseOnHover
+                autoClose={1500}
+            />
             <Navbar />
             <main className='mb-auto w-10/12 max-w-4xl mx-auto'>
                 <Outlet />
